46493becaeca80821ffe36c2d92aa38c92d4ed3d
Aidan <aidannn@seas.upenn.edu>
2026-01-04 23:04:57 -0500

    Del

--- Assets/Shaders/Includes/PostProcessHelp.hlsl
+++
@@ -1,6 +0,0 @@
-void Tint_float(float3 SceneColor, float3 TintColor, out float3 Color) {
-    const float3 colToGray = float3(0.2126, 0.7152, 0.0722); // From https://gmshaders.com/tutorials/basic_colors/
-
-    float gray = dot(SceneColor, colToGray);
-    Color = TintColor * gray;
-}
--- Assets/Shaders/Includes/TimeHelp.hlsl
+++
@@ -1,4 +0,0 @@
-void PosterizeTime_float(float Time, float Framerate, out float PosterizedTime) {
-    float repTime = 1./Framerate;
-    PosterizedTime = floor(Time/repTime)*repTime;
-}
